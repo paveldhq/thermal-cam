b261c1cf00fed760a27ecf076faa9f197e6b533a
Pavlo Ratushnyi <paveldhq@users.noreply.github.com>
2020-01-31 20:08:50 +0200

    move files out of code

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -2,110 +2,46 @@
  * Rui Santos
  * Complete Project Details https://randomnerdtutorials.com
  */
- 
+// https://github.com/me-no-dev/ESPAsyncWebServer
+//https://github.com/me-no-dev/AsyncTCP
+
+#include "ESPAsyncWebServer.h"
 #include <WiFi.h>
 #include <WiFiClient.h>
-#include <WebServer.h>
+//#include <WebServer.h>
 #include <ESPmDNS.h>
 #include <Update.h>
+#include "SPIFFS.h"
+
  
 const char* host = "esp32";
 const char* ssid = "esp_test";
 const char* password = "12345679";
  
-WebServer server(80);
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+//WebServer server(80);
  
 /*
  * Login page
  */
-const char* loginIndex =
- "<form name='loginForm'>"
-    "<table width='20%' bgcolor='A09F9F' align='center'>"
-        "<tr>"
-            "<td colspan=2>"
-                "<center><font size=4><b>ESP32 Login Page</b></font></center>"
-                "<br>"
-            "</td>"
-            "<br>"
-            "<br>"
-        "</tr>"
-        "<td>Username:</td>"
-        "<td><input type='text' size=25 name='userid'><br></td>"
-        "</tr>"
-        "<br>"
-        "<br>"
-        "<tr>"
-            "<td>Password:</td>"
-            "<td><input type='Password' size=25 name='pwd'><br></td>"
-            "<br>"
-            "<br>"
-        "</tr>"
-        "<tr>"
-            "<td><input type='submit' onclick='check(this.form)' value='Login'></td>"
-        "</tr>"
-    "</table>"
-"</form>"
-"<script>"
-    "function check(form)"
-    "{"
-    "if(form.userid.value=='admin' && form.pwd.value=='admin')"
-    "{"
-    "window.open('/serverIndex')"
-    "}"
-    "else"
-    "{"
-    " alert('Error Password or Username')/*displays error message*/"
-    "}"
-    "}"
-"</script>";
- 
+//const char* loginIndex = "";
 /*
  * Server Index Page
  */
  
-const char* serverIndex =
-"<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
-"<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
-   "<input type='file' name='update'>"
-        "<input type='submit' value='Update'>"
-    "</form>"
- "<div id='prg'>progress: 0%</div>"
- "<script>"
-  "$('form').submit(function(e){"
-  "e.preventDefault();"
-  "var form = $('#upload_form')[0];"
-  "var data = new FormData(form);"
-  " $.ajax({"
-  "url: '/update',"
-  "type: 'POST',"
-  "data: data,"
-  "contentType: false,"
-  "processData:false,"
-  "xhr: function() {"
-  "var xhr = new window.XMLHttpRequest();"
-  "xhr.upload.addEventListener('progress', function(evt) {"
-  "if (evt.lengthComputable) {"
-  "var per = evt.loaded / evt.total;"
-  "$('#prg').html('progress: ' + Math.round(per*100) + '%');"
-  "}"
-  "}, false);"
-  "return xhr;"
-  "},"
-  "success:function(d, s) {"
-  "console.log('success!')"
- "},"
- "error: function (a, b, c) {"
- "}"
- "});"
- "});"
- "</script>";
+//const char* serverIndex = "";
  
 /*
  * setup function
  */
 void setup(void) {
   Serial.begin(115200);
- 
+ if(!SPIFFS.begin(true)){
+  Serial.println("An Error has occurred while mounting SPIFFS");
+  return;
+}
   // Connect to WiFi network
   //WiFi.begin(ssid, password);
  WiFi.softAP(ssid, password);
@@ -124,17 +60,22 @@ void setup(void) {
   }
   Serial.println("mDNS responder started");
   /*return index page which is stored in serverIndex */
-  server.on("/", HTTP_GET, []() {
-    server.sendHeader("Connection", "close");
-    server.send(200, "text/html", loginIndex);
+
+ server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/login.html", "text/html");
   });
-  server.on("/serverIndex", HTTP_GET, []() {
-    server.sendHeader("Connection", "close");
-    server.send(200, "text/html", serverIndex);
+
+   server.on("/server.html", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/server.html", "text/html");
   });
+
+
   /*handling uploading firmware file */
-  server.on("/update", HTTP_POST, []() {
-    server.sendHeader("Connection", "close");
+  /*
+  server.on("/update", HTTP_POST, [](AsyncWebServerRequest *request){
+    AsyncWebServerResponse *response = request->beginResponse_P(200, "text/html", index_html);
+    response->addHeader("Connection", "close");
+    
     server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
     ESP.restart();
   }, []() {
@@ -145,7 +86,7 @@ void setup(void) {
         Update.printError(Serial);
       }
     } else if (upload.status == UPLOAD_FILE_WRITE) {
-      /* flashing firmware to ESP*/
+      // flashing firmware to ESP
       if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
         Update.printError(Serial);
       }
@@ -157,10 +98,12 @@ void setup(void) {
       }
     }
   });
+*/
+  
   server.begin();
 }
  
 void loop(void) {
-  server.handleClient();
-  delay(1);
+  //server.handleClient();
+  //delay(1);
 }
